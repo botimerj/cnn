c507775fb4108eb3a2038f1a978a4de9a3cd1c5b
Jake <botimerj@umich.edu>
2018-06-08 19:10:11 -0400

    improved conv_tb

--- tb/conv_tb.cpp
+++ tb/conv_tb.cpp
@@ -20,19 +20,23 @@ void read_fp_array(ifstream& my_file, float* float_arr, int* x, int* y){
     if(my_file.is_open()){
         i = 0; *y = 0;
         while(getline(my_file,line)){
-            while(line.length()>0){
-                pos = line.find(",");
-                if(pos != string::npos){
-                    num = line.substr(0,pos);
-                    line = line.substr(pos+1);
-                    float_arr[i++] = atof(num.c_str());
-                }else{
-                    num = line;
-                    line = string("");
-                    float_arr[i++] = atof(num.c_str());
+            if(line != "N"){
+                while(line.length()>0){
+                    pos = line.find(",");
+                    if(pos != string::npos){
+                        num = line.substr(0,pos);
+                        line = line.substr(pos+1);
+                        float_arr[i++] = atof(num.c_str());
+                    }else{
+                        num = line;
+                        line = string("");
+                        float_arr[i++] = atof(num.c_str());
+                    }
                 }
+                (*y)++;
+            }else{
+                break;
             }
-            (*y)++;
         }
     }
     *x = i / *y;
@@ -41,51 +45,67 @@ void read_fp_array(ifstream& my_file, float* float_arr, int* x, int* y){
 int main(){ 
     
     cout << "Convolution Testbench : ";
-
-    float A[100];
-    int ax, ay;
     ifstream f_file("f.txt");
-    read_fp_array(f_file, A, &ax, &ay);
-    f_file.close();
-
-    float B[100];
-    int bx, by;
     ifstream g_file("g.txt");
-    read_fp_array(g_file, B, &bx, &by);
-    g_file.close();
-
-
-
-    float C_check[361];
-    int cx_check, cy_check;
     ifstream h_file("h.txt");
-    read_fp_array(h_file, C_check, &cx_check, &cy_check);
-    h_file.close();
 
-    int cx = ax + bx - 1;
-    int cy = ay + by - 1;
-    float C[cx*cy];
-    conv2d(A, B, C, ax, ay, bx, by, cx, cy);  
+    for(int test_num = 0; test_num < 5; test_num++){
+        float A[100];
+        int ax, ay;
+        read_fp_array(f_file, A, &ax, &ay);
+        //f_file.close();
+
+        float B[100];
+        int bx, by;
+        read_fp_array(g_file, B, &bx, &by);
+        //g_file.close();
 
 
 
+        float C_check[361];
+        int cx_check, cy_check;
+        read_fp_array(h_file, C_check, &cx_check, &cy_check);
+        //h_file.close();
 
-    if(cx_check != cx || cy_check != cy) {
-        cout << "Error: dimension mismatch" << endl;
-        return 1;
-    }
+        int cx = ax + bx - 1;
+        int cy = ay + by - 1;
+        float C[cx*cy];
+        conv2d(A, B, C, ax, ay, bx, by, cx, cy);  
 
-    for(int j = 0; j < cy; j++){
-        for(int i = 0; i < cx; i++){
-            if(abs((C[j*cx + i] - C_check[j*cx + i])) > 0.0001){
-                cout << "Error: convolution mismatch" << endl;
-                cout << "element (" << i << "," << j << ") = " << C[j*cx+i] << " vs " << C_check[j*cx+i] << endl;
-                return 1;
+
+
+        
+
+        if(cx_check != cx || cy_check != cy) {
+            cout << "Error: dimension mismatch" << endl;
+            return 1;
+        }
+
+        for(int j = 0; j < cy; j++){
+            for(int i = 0; i < cx; i++){
+                if(abs((C[j*cx + i] - C_check[j*cx + i])) > 0.0001){
+                    cout << "Error: convolution mismatch" << endl;
+                    cout << "element (" << i << "," << j << ") = " << C[j*cx+i] << " vs " << C_check[j*cx+i] << endl;
+                    return 1;
+                }
             }
         }
-    }
+
+        // print result 
+        //for(int j = 0; j < cy; j++){
+        //    for(int i = 0; i < cx; i++){
+        //        cout << C[j*cx + i] << " ";
+        //        }
+        //    cout << endl;
+        //}
+    } // End test loop
+
+    f_file.close();
+    g_file.close();
+    h_file.close();
 
     cout << "Pass" << endl;
+
     return 0;
 
 }
