4d53b5dabe585a68bde678cfec9af62ae2dc6a81
Jake <botimerj@umich.edu>
2018-06-08 18:49:28 -0400

    updated conv_truth.m

--- tb/conv_truth.m
+++ tb/conv_truth.m
@@ -1,14 +1,45 @@
-% Convolution Truth
-function conv_truth(max_dim) 
-    f_dims = randi(max_dim,1,2);
-    g_dims = randi(max_dim,1,2);
-    
-    f = rand(f_dims(1), f_dims(2));
-    g = rand(g_dims(1), g_dims(2));
-    
+% Convolution Practice
+function conv_practice
+    new_test = 'N';
+
+    %Special cases
+    %One Element test
+    f = [1];
+    g = [1];
     h = conv2(f,g);
+    dlmwrite('f.txt',f); dlmwrite('f.txt',new_test,'-append');
+    dlmwrite('g.txt',g); dlmwrite('g.txt',new_test,'-append');
+    dlmwrite('h.txt',h); dlmwrite('h.txt',new_test,'-append');
     
-    dlmwrite('f.txt',f);
-    dlmwrite('g.txt',g);
-    dlmwrite('h.txt',h);
+    %One dimension test
+    f = [1, 2, 3];
+    g = [3, 4, 5];
+    h = conv2(f,g);
+    dlmwrite('f.txt',f,'-append'); dlmwrite('f.txt',new_test,'-append');
+    dlmwrite('g.txt',g,'-append'); dlmwrite('g.txt',new_test,'-append');
+    dlmwrite('h.txt',h,'-append'); dlmwrite('h.txt',new_test,'-append');
+    
+    %One dimension (alternating dimensions) test
+    f = [1, 2, 3];
+    g = [3; 4; 5];
+    h = conv2(f,g);
+    dlmwrite('f.txt',f,'-append'); dlmwrite('f.txt',new_test,'-append');
+    dlmwrite('g.txt',g,'-append'); dlmwrite('g.txt',new_test,'-append');
+    dlmwrite('h.txt',h,'-append'); dlmwrite('h.txt',new_test,'-append');
+    
+    %Random testing
+    for i = 1:2
+        f_dims = randi(10,1,2);
+        g_dims = randi(10,1,2);
+
+        f = rand(f_dims(1), f_dims(2));
+        g = rand(g_dims(1), g_dims(2));
+
+        h = conv2(f,g);
+ 
+        dlmwrite('f.txt',f,'-append'); dlmwrite('f.txt',new_test,'-append');
+        dlmwrite('g.txt',g,'-append'); dlmwrite('g.txt',new_test,'-append');
+        dlmwrite('h.txt',h,'-append'); dlmwrite('h.txt',new_test,'-append');
+              
+    end
 end
